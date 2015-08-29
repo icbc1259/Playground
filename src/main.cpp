c36131e979dfeee411d5f3f31c54c99a9e6803ab
icbc1259 <icbc1259@gmail.com>
2015-08-29 15:49:29 +0800

    add words

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,6 @@ int main(int argc, char const *argv[])
 {
 	cout<<"hello world!"<< endl;
 	cout<<"this line is create at branch 2"<< endl;
+	cout<<"add another words at branch 2"<< endl;
 	return 0;
 }
