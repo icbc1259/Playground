8b1a6044e71067be7ba83c65fe078fc73076b8ed
icbc1259 <icbc1259@gmail.com>
2015-08-29 16:29:53 +0800

    master!!!

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,6 @@ int main(int argc, char const *argv[])
 	cout<<"hello world!"<< endl;
 	cout<<"this line is create at branch 2"<< endl;
 	cout<<"add another words at branch 2"<< endl;
+	cout<<"where is the branch master!!!!"<< endl;
 	return 0;
 }
